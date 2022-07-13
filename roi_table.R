2704b7e35040e0fe493ec5f662ace617c042d964
Joe Omatoi <joepopop@pa-dhcp-10-120-235-114.gp-vpn.northwestern.private>
2022-07-13 00:02:34 -0500

    update

--- roi_table.R
+++ roi_table.R
@@ -1,10 +1,14 @@
 library(readxl)
+library(tidyverse)
 
 read_excel("roi_table.xlsx") %>% 
   filter(ROI > 0) %>% 
   arrange(desc(ROI)) %>% 
   arrange(desc(Matches)) %>%
-  mutate(ROI = ROI*100) %>% 
+  mutate(ROI = round(ROI*100, 2)) %>% 
   rename("ROI (%)" = ROI) %>% 
+  rename("Cost per match ($)" = "Cost per match") %>% 
+  rename("Profit ($)" = Profit) %>% 
+  rename("Total Cost ($)" = "Total Cost") %>% 
   gt::gt() %>% 
   gt::tab_options(table.font.names = "Optima")
